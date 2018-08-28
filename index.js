05f89b5ffce2685773cd743a880bd3d9714c4bbd
ritz-w <ritz.wu@outlook.com>
2018-08-28 10:58:00 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  //this is where we can use conditionals given our argument, someValue
+  //don't forget to return whatever the result is!
+  let result;
+  if (someValue <= 400) {
+    result = 'This one is on me!';
+    }
+    else if (someValue > 2000 && someValue < 2500) {
+      result = 'I will gladly take your thirty bucks.';
+    }
+    else if (someValue > 2500) {
+      result = 'No can do.';
+    }
+    return result;
+  };
+
+
+function ternaryCheckCity(city) {
+  const isCity = (city === 'NYC') ? true : false;
+  if (isCity === true) {
+    return "Ok, sounds good.";
+  }
+  else {
+    return "No go.";
+  }
+}
+
+function switchOnCharmFromTip(generosity) {
+  let answer;
+  switch (generosity) {
+    case 'generous':
+    answer = "Thank you so much.";
+    break;
+    case 'not as generous':
+    answer = "Thank you.";
+    break;
+    default:
+    answer = 'Bye.';
+    break;
+  }
+  return answer;
+}
